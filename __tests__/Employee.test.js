8b2f6acd7d883c0b620b8c2d811c4bc9273c4332
Matthew Pandolfo <matt@pandolfo.com>
2021-09-25 21:19:39 -0400

    Engineer test pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,12 @@
 const { TestWatcher } = require("@jest/core");
 const Employee = require("../lib/Employee");
 
-test('creates a test employee', () => {
-  const employee = new Employee('Syk', 5, 'Email', 2)
+test("creates a test employee", () => {
+  const employee = new Employee("Syk", 5, "Email", 2);
 
-  expect(employee.name).toEqual(expect.any(String))
-  expect(employee.job).toBe('Employee')
-  expect(employee.id).toEqual(expect.any(Number))
-  expect(employee.email).toEqual(expect.any(String))
-  expect(employee.officeNum).toEqual(expect.any(Number))
-})
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.job).toBe("Employee");
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.officeNum).toEqual(expect.any(Number));
+});
